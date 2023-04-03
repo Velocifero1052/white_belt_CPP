08403c18684e00571fbc7ea78f8b224c7a13c03d
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-03 17:22:03 +0500

    higher than average task completed

--- main.cpp
+++ main.cpp
@@ -2,21 +2,41 @@
 //#include "headers/solutions.h"
 
 #include <iostream>
+#include <vector>
+#include <numeric>
 
-int Factorial(int a){
-    if(a < 0){
-        return 1;
-    }
-    int res = 1;
-    for(int i = 1; i <= a; i++){
-        res *= i;
-    }
-    return res;
-}
-
+using namespace std;
 
 int main() {
+    int size_of_vector;
+    cin >> size_of_vector;
+    vector<int> temps(size_of_vector);
 
+    for(int i = 0; i < size_of_vector; i++){
+        int temp;
+        cin >> temp;
+        temps[i] = temp;
+    }
+
+    int common_value = accumulate(temps.begin(), temps.end(), 0) / temps.size();
+    vector<int>higher_that_average;
+
+    for(int i = 0; i < size_of_vector; i++){
+        if(temps[i] > common_value)
+            higher_that_average.push_back(i);
+    }
+
+
+    cout << higher_that_average.size() << endl;
+    bool first = true;
+    for(auto elem: higher_that_average){
+        if(first){
+            cout << elem;
+            first = false;
+        }else{
+            cout << " " << elem;
+        }
+    }
 
     return 0;
 }
