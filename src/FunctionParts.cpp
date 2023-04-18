7e42e44c6aec6a947c2364dd651ef23fea65b832
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-18 12:13:59 +0500

    function parts completed

--- src/FunctionParts.cpp
+++ src/FunctionParts.cpp
@@ -1,14 +1,8 @@
-#include <algorithm>
-#include <iostream>
-#include <map>
+//
+// Created by Legion on 4/18/2023.
+//
 #include <vector>
-
-using std::string;
-using std::reverse;
-using std::cout;
-using std::endl;
-using std::map;
-using std::vector;
+#include <algorithm>
 
 class FunctionPart{
 public:
@@ -61,16 +55,40 @@ public:
         return value;
     }
 private:
-    vector<FunctionPart> parts;
+    std::vector<FunctionPart> parts;
 };
 
 
+struct Image {
+    double quality;
+    double freshness;
+    double rating;
+};
 
-int main() {
-    Image image = {10, 2, 6};
-    Params params = {4, 2, 6};
-    cout << ComputeImageWeight(params, image) << endl;
-    cout << ComputeQualityByWeight(params, image, 52) << endl;
-    return 0;
+struct Params {
+    double a;
+    double b;
+    double c;
+};
+
+Function MakeWeightFunction(const Params& params,
+                            const Image& image) {
+    Function function;
+    function.AddPart('*', params.a);
+    function.AddPart('-', image.freshness * params.b);
+    function.AddPart('+', image.rating * params.c);
+    return function;
 }
 
+double ComputeImageWeight(const Params& params, const Image& image) {
+    Function function = MakeWeightFunction(params, image);
+    return function.Apply(image.quality);
+}
+
+double ComputeQualityByWeight(const Params& params,
+                              const Image& image,
+                              double weight) {
+    Function function = MakeWeightFunction(params, image);
+    function.Invert();
+    return function.Apply(weight);
+}
