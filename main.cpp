6ee2dfdc683712b73aeba51625db9d27fde7d615
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-18 09:57:14 +0500

    person 3 task completed

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
-
-#include <utility>
-#include <vector>
 #include <algorithm>
-#include <string>
 #include <iostream>
+#include <string>
 #include <map>
+
+
 using std::string;
 using std::reverse;
 using std::cout;
 using std::endl;
+using std::map;
+
 
 
 int main() {
+
 
 
     return 0;
