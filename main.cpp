820daa2498fddb21abc13fe1339b437e127faeb6
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-18 09:59:29 +0500

    incognizable task completed

--- main.cpp
+++ main.cpp
@@ -10,11 +10,18 @@ using std::cout;
 using std::endl;
 using std::map;
 
+struct Incognizable{
+    int a;
+    int b;
+};
 
 
 int main() {
 
-
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
 
     return 0;
 }
---
+++ src/incognizable.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Legion on 4/18/2023.
+//
+struct Incognizable{
+    int a;
+    int b;
+};
