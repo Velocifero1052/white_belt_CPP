782390d44012515a2863e366bd688fdd80ff1f14
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-20 11:48:34 +0500

    Rational second and third part completed

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 using std::string;
 using std::vector;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::istringstream;
+using std::ostringstream;
 
 class Rational {
 public:
@@ -23,14 +26,7 @@ public:
         int a_num = abs(numerator_);
         int a_den = abs(denominator_);
 
-        int common_divider = gcd(a_num, a_den);
-
-        if(common_divider != 1){
-            while(a_num % common_divider == 0 && a_den % common_divider == 0){
-                a_num /= common_divider;
-                a_den /= common_divider;
-            }
-        }
+        shorten_for_common(a_num, a_den);
 
         numerator = a_num;
         denominator = a_den;
@@ -57,6 +53,48 @@ public:
         return denominator;
     }
 
+    Rational operator+(const Rational& rhs){
+        if(this->denominator == rhs.denominator) {
+            this->numerator += rhs.numerator;
+            return {this->numerator, this->denominator};
+        }else{
+            int common_multiple = lcm(this->denominator, rhs.denominator);
+            int left_to_multiply = common_multiple / this->denominator;
+            int right_to_multiply = common_multiple / rhs.denominator;
+            this->numerator *= left_to_multiply;
+            int right_numerator = rhs.numerator * right_to_multiply;
+            int res = this->numerator + right_numerator;
+            return {res, common_multiple};
+        }
+    }
+
+    Rational operator-(const Rational& rhs){
+        if(this->denominator == rhs.denominator)
+            this->numerator -= rhs.numerator;
+        else{
+            int common_multiple = lcm(this->denominator, rhs.denominator);
+            int left_to_multiply = common_multiple / this->denominator;
+            int right_to_multiply = common_multiple / rhs.denominator;
+            this->numerator *= left_to_multiply;
+            int right_numerator = rhs.numerator * right_to_multiply;
+            int res = this->numerator - right_numerator;
+            return {res, common_multiple};
+        }
+        return {this->numerator, this->denominator};
+    }
+
+    Rational operator*(const Rational& rhs) const{
+        return {this->numerator * rhs.numerator, this->denominator * rhs.denominator};
+    }
+
+    Rational operator/(const Rational& rhs)const{
+        return {this->numerator * rhs.denominator, this->denominator * rhs.numerator};
+    }
+
+    bool operator==(const Rational& rhs) const{
+        return this->numerator == rhs.numerator && this->denominator == rhs.denominator;
+    }
+
 private:
     // Добавьте поля
     int numerator;
@@ -68,9 +106,26 @@ private:
         return gcd(b, a % b);
     }
 
+    int lcm(int a, int b){
+        int temp = gcd(a, b);
+        return temp ? (a / temp * b) : 0;
+    }
+
+    void shorten_for_common(int& a_num, int& a_den){
+        int common_divider = gcd(a_num, a_den);
+
+        if(common_divider != 1){
+            while(a_num % common_divider == 0 && a_den % common_divider == 0){
+                a_num /= common_divider;
+                a_den /= common_divider;
+            }
+        }
+    }
+
 };
 
 int main() {
+
 
     {
         const Rational r(3, 10);
@@ -124,6 +179,60 @@ int main() {
         if (defaultConstructed.Numerator() != 0 || defaultConstructed.Denominator() != 1) {
             cout << "Rational() != 0/1" << endl;
             return 5;
+        }
+    }
+
+    {
+        Rational r1(4, 6);
+        Rational r2(2, 3);
+        bool equal = r1 == r2;
+        if (!equal) {
+            cout << "4/6 != 2/3" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal) {
+            cout << "2/3 + 4/3 != 2" << endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal) {
+            cout << "5/7 - 2/9 != 31/63" << endl;
+            return 3;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a * b;
+        bool equal = c == Rational(8, 9);
+        if (!equal) {
+            cout << "2/3 * 4/3 != 8/9" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(5, 4);
+        Rational b(15, 8);
+        Rational c = a / b;
+        bool equal = c == Rational(2, 3);
+        if (!equal) {
+            cout << "5/4 / 15/8 != 2/3" << endl;
+            return 2;
         }
     }
 
--- src/Rational.cpp
+++ src/Rational.cpp
@@ -2,6 +2,7 @@
 // Created by Legion on 4/20/2023.
 //
 #include <algorithm>
+
 class Rational {
 public:
     Rational() {
@@ -17,14 +18,7 @@ public:
         int a_num = abs(numerator_);
         int a_den = abs(denominator_);
 
-        int common_divider = gcd(a_num, a_den);
-
-        if(common_divider != 1){
-            while(a_num % common_divider == 0 && a_den % common_divider == 0){
-                a_num /= common_divider;
-                a_den /= common_divider;
-            }
-        }
+        shorten_for_common(a_num, a_den);
 
         numerator = a_num;
         denominator = a_den;
@@ -51,6 +45,48 @@ public:
         return denominator;
     }
 
+    Rational operator+(const Rational& rhs){
+        if(this->denominator == rhs.denominator) {
+            this->numerator += rhs.numerator;
+            return {this->numerator, this->denominator};
+        }else{
+            int common_multiple = lcm(this->denominator, rhs.denominator);
+            int left_to_multiply = common_multiple / this->denominator;
+            int right_to_multiply = common_multiple / rhs.denominator;
+            this->numerator *= left_to_multiply;
+            int right_numerator = rhs.numerator * right_to_multiply;
+            int res = this->numerator + right_numerator;
+            return {res, common_multiple};
+        }
+    }
+
+    Rational operator-(const Rational& rhs){
+        if(this->denominator == rhs.denominator)
+            this->numerator -= rhs.numerator;
+        else{
+            int common_multiple = lcm(this->denominator, rhs.denominator);
+            int left_to_multiply = common_multiple / this->denominator;
+            int right_to_multiply = common_multiple / rhs.denominator;
+            this->numerator *= left_to_multiply;
+            int right_numerator = rhs.numerator * right_to_multiply;
+            int res = this->numerator - right_numerator;
+            return {res, common_multiple};
+        }
+        return {this->numerator, this->denominator};
+    }
+
+    Rational operator*(const Rational& rhs) const{
+        return {this->numerator * rhs.numerator, this->denominator * rhs.denominator};
+    }
+
+    Rational operator/(const Rational& rhs)const{
+        return {this->numerator * rhs.denominator, this->denominator * rhs.numerator};
+    }
+
+    bool operator==(const Rational& rhs) const{
+        return this->numerator == rhs.numerator && this->denominator == rhs.denominator;
+    }
+
 private:
     // Добавьте поля
     int numerator;
@@ -60,6 +96,22 @@ private:
         if (b == 0)
             return a;
         return gcd(b, a % b);
+    }
+
+    int lcm(int a, int b){
+        int temp = gcd(a, b);
+        return temp ? (a / temp * b) : 0;
+    }
+
+    void shorten_for_common(int& a_num, int& a_den){
+        int common_divider = gcd(a_num, a_den);
+
+        if(common_divider != 1){
+            while(a_num % common_divider == 0 && a_den % common_divider == 0){
+                a_num /= common_divider;
+                a_den /= common_divider;
+            }
+        }
     }
 
 };
